957e603f9848a484cae3adb74ae213ddea0325db
Ranmal Nanayakkara <udayanga.2015253@iit.ac.lk>
2016-12-06 12:53:27 +0530

    Improving the msg

--- Motion.py
+++ Motion.py
@@ -1,16 +1,18 @@
  print "Ultrasonic Measurement"
  
  try:
+   count=0
    lid_status=True
 
          pir_Input=GPIO.input(11)
- 
+
         if pir_Input==1 and pirStat:
-            print "Life Detected "            
+            print "Life Detected ",count              #When output from motion sensor is HIGH
         else:
-            print "No Any Life Detected"
+            print "No Any Life Detected",count
  
         time.sleep(2)
+        count=count+1
  
  except KeyboardInterrupt:
    GPIO.cleanup()
