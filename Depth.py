38c12addec97bb0c1d1ed8664d86195e06cbb013
Sonal <sonal.2015347@iit.ac.lk>
2017-01-06 08:56:41 +0530

    final version depth

--- Depth.py
+++ Depth.py
@@ -8,13 +8,11 @@
 
 def measure():
   # This function measures a distance
+  GPIO.output(TRIGGER_PIN, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIGGER_PIN, False)
+  start = time.time()
 
-	GPIO.output(TRIGGER_PIN, True)
-	time.sleep(0.00001)
-	GPIO.output(TRIGGER_PIN, False)
-	start = time.time()
-
-  
   while GPIO.input(ECHO_PIN)==0:
     start = time.time()
 
@@ -26,23 +24,20 @@ def measure():
   distance = (elapsed * 34300)/2
 
   return distance
-  #This function measure a average.to improve the accuracy
+  
+#This function measure a average.to improve the accuracy
 
 def measure_average(): 
- try:
   count=0
   distance=0;
   while (count<3):
 	distance1=measure()
-	time.sleep(.0005)
+	time.sleep(.01)
 	distance=distance+distance1
 	count=count+1
-   except:
-	print "Exception"
  
   distance=distance/3
   return distance
-
 
 # -----------------------
 # Main Script
@@ -53,21 +48,32 @@ def measure_average():
 GPIO.setmode(GPIO.BCM)
 
 #defining the GPIO pin for the trigger and echo
-TRIGGER_PIN = 33
-ECHO_PIN    = 35
-  try:
-	GPIO.setup(TRIGGER_PIN,GPIO.OUT)  # Trigger
-	GPIO.setup(ECHO_PIN,GPIO.IN)      # Echo
-	GPIO.output(TRIGGER_PIN, False)
-   except:
-	print "Exception"
-	
+TRIGGER_PIN = 23
+ECHO_PIN    = 24
+
+print "Ultrasonic Measurement"
+
+GPIO.setup(TRIGGER_PIN,GPIO.OUT)  # Trigger
+GPIO.setup(ECHO_PIN,GPIO.IN)      # Echo
+GPIO.output(TRIGGER_PIN, False)
+
+
 #Firebase Configuration
 fire = firebase.FirebaseApplication('https://zbin-6eb81.firebaseio.com/')
 
 try:
 
+  while True:
 
+    distance = measure_average()
+    print "Distance : %.1f" % distance
+    stringDistance = str(distance)
+    try:
+           result = fire.put('level','currentLevel',stringDistance)
+    except:
+           print "connection failed"
+
+    time.sleep(1)
 
 except KeyboardInterrupt:
   GPIO.cleanup()
